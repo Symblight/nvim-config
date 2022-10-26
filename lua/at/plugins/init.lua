ed7fed817495ba9522048c000b6afadb071fb801
Alexey <symblight@gmail.com>
2022-10-26 11:13:37 +0300

    add git

--- lua/at/plugins/init.lua
+++ lua/at/plugins/init.lua
@@ -1,8 +1,7 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path}) end
 
 
 return require("packer").startup(function(use) 
@@ -62,6 +61,7 @@ return require("packer").startup(function(use)
 
   -- Git
   use "lewis6991/gitsigns.nvim"
+  use "tpope/vim-fugitive"
 
   -- Telescope
   use {
